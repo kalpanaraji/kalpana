5ad81a97ab422eef5010c5950bec557cbc0a8f91
kalpanaraji <40694498+kalpanaraji@users.noreply.github.com>
2018-07-02 21:45:38 +0530

    Create natural.java

---
+++ natural.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+class natural
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int k=sc.nextInt();
+        int sum=0;
+        for(int i=0;i<=k;i++)
+        {
+            sum=sum+i;
+        }
+        System.out.print(sum);
+        
+    }
+
+}
+
