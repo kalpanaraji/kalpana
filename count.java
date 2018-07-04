e2cbcae70e3d48424d21b0755d2a18dfec211bd0
kalpanaraji <40694498+kalpanaraji@users.noreply.github.com>
2018-07-04 21:20:19 +0530

    Create count.java

---
+++ count.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+class count
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n1= sc.nextInt();
+        int b=Integer.toString(n1).length();
+        System.out.print(b);
+        
+        
+    }
+}
